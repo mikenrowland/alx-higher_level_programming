2f722afc34c3ffe386ef94350e2d2ae1e6c92f2d
Michael Ngwube <mykhelmyers@gmail.com>
2022-09-27 23:37:04 +0000

    print ints in a nested list/matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         for x in range(i):
             for y in range(len(matrix[x])):
                 if y == len(matrix[x]) - 1:
-                    print("{:d}".format(matrix[x][y]), end="$")
+                    print("{:d}".format(matrix[x][y]), end="")
                 else:
                     print("{:d}".format(matrix[x][y]), end=" ")
             print()
-    else:
-        print('$')
