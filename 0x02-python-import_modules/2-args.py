cbc3facd9d2e5f77f33ce8cdc50fc999714e57ba
Michael Ngwube <mykhelmyers@gmail.com>
2023-02-04 10:05:24 +0000

    Dynamic script that prints number of function args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 def print_number_of_args(argv):
     args_count = len(argv) - 1
     if args_count == 0:
-        print("{:d} argument.".format(args_count))
+        print("{:d} arguments.".format(args_count))
         return
     else:
         if args_count == 1:
@@ -13,6 +13,7 @@ def print_number_of_args(argv):
         while i <= args_count:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
+
 
 if __name__ == "__main__":
     import sys
